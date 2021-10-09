ff733a8da69e695a05a5c7f511345a89a114423a
LeinysPaola <lepagifo@hotmail.com>
2021-10-08 23:05:56 -0500

    leinys

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { ClienteRoutes} from './cliente'
 import {ProductoRoutes} from './producto'
+
 import { DistribuidoRoutes } from './distribuidos';
+
 export class Routes {
     public clienteRoutes: ClienteRoutes = new ClienteRoutes();
     public productoRoutes: ProductoRoutes = new ProductoRoutes();
+
     public distribuidoRoutes: DistribuidoRoutes = new DistribuidoRoutes();
 }
 
