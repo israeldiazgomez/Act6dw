1ebfdd85d53824d638fbd6cfd6d103942a7a4435
israel25D <ijesusdiaz@uniguajira.edu.co>
2021-10-08 21:52:43 -0500

    israel

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { ClienteRoutes} from './cliente'
-
+import {ProductoRoutes} from './producto'
 export class Routes {
     public clienteRoutes: ClienteRoutes = new ClienteRoutes();
+    public productoRoutes: ProductoRoutes = new ProductoRoutes();
 }
+
 
