5763d18d0d746fb08cf3c737c924a39235dd20d7
leinys <lepagifo@hotmail.com>
2021-10-09 00:00:11 -0500

    leinys

--- src/config/index.ts
+++ src/config/index.ts
@@ -27,7 +27,8 @@ export class App {
 
     private routes(){
         this.routePrv.clienteRoutes.routes(this.app);
-        this.routePrv.productoRoutes.routes(this.app)
+        this.routePrv.productoRoutes.routes(this.app);
+        this.routePrv.distribuidosRoutes.routes(this.app);
     }
 
     async listen(){
